b3da716b10aa45d95b17813bf6adbfa54b53b72d
MCFreeman <86325150+MCFreeman@users.noreply.github.com>
2023-10-09 22:14:20 +0200

    w

--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -22,13 +22,13 @@ const Hero = () => {
                 </p>
                 <div className="flex flex-col items-center justify-center space-y-4 sm:flex-row sm:space-x-4 sm:space-y-0">
                   <Link
-                    href="/blogg"
+                    href="/blog"
                     className="rounded-md bg-primary py-4 px-8 text-base font-semibold text-white duration-300 ease-in-out hover:bg-primary/80"
                   >
-                    Blogg
+                    Blog
                   </Link>
                   <Link
-                    href="/video"
+                    href="/features"
                     className="rounded-md bg-black/20 py-4 px-8 text-base font-semibold text-black duration-300 ease-in-out hover:bg-black/30 dark:bg-white/20 dark:text-white dark:hover:bg-white/30"
                   >
                     Prosjekter
